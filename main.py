d429d97336588368469e115f13faa356c3fcdc3f
fulano25 <sinofpi@gmail.com>
2019-03-15 10:03:56 -0300

    implemented schedule scrapping with bs4

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from requests_html import HTMLSession
+from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from private import *
-
+import os  
 
 
 PATH = {
@@ -19,12 +20,15 @@ PATH = {
     'historico_escolar_cr_aprovados': '/aluno/aluno/relatorio/com/historicoEscolarCRAprovados.action?matricula=',
 
     'horarios': '/aluno/aluno/quadrohorario/menu.action?matricula=',
+    'quadro_horario': '/aluno/ajax/aluno/quadrohorario/quadrohorario.action?matricula=',
     'notas': '/aluno/aluno/nota/nota.action?matricula=',
     'comunicados': '/comunicacoes/noticia/list.action?br.com.asten.si.geral.web.spring.interceptors.AplicacaoWebChangeInterceptor.aplicacaoWeb=1',
 
 }
 
 SITE = 'https://alunos.cefet-rj.br/'
+
+REL_DIR = './rel'
 
 class Session(HTMLSession):
     def __init__(self, url=SITE):
@@ -34,15 +38,21 @@ class Session(HTMLSession):
         self.username = USERNAME
         self.matricula = None
         self.home = self.login()
-        try:
-            matricula_element = self.home.html.find('#matricula', first=True)
-            if matricula_element:
-                self.matricula = matricula_element.attrs['value']
-        except Exception as ex:
-            print(ex)
-        comp = ['atestado_trancamento', 'boletim_escolar', 'boletim_escolar_tecnico', 'comprovante_matricula', 'ficha_cadastral', 'integralizacao_curricular', 'solicitacao_matricula', 'comprovante_matricula_sie', 'historico_escolar_cr_aprovados']
-        for f in comp:
-            self.save_file(f)
+        
+        if not os.path.isdir(REL_DIR):
+            os.makedirs(REL_DIR)
+        
+
+        matricula_element = self.home.html.find('#matricula', first=True)
+        if matricula_element:
+            self.matricula = matricula_element.attrs['value']
+        else:
+            raise Exception('matrícula não encontrada')
+
+        self.print_table()
+        # comp = list(PATH.keys())[2:11]
+        # for f in comp:
+        #     self.save_pdf(f)
 
     def login(self):
         """
@@ -59,11 +69,41 @@ class Session(HTMLSession):
         url = urljoin(self.home.url, PATH[path] + self.matricula)
         return self.get(url)
     
-    def save_file(self, file_name):
+    def save_pdf(self, file_name):
         response = self.get_page(file_name)
-        with open(file_name + '.pdf', 'wb') as f:
-            f.write(response.content)
+        content_type = response.headers.get('content-type')
+        name = os.path.join(REL_DIR, file_name + '.pdf')
+        if 'application/pdf' in content_type:
+            with open(name, 'wb') as f:
+                f.write(response.content)
+        else:
+            print('{}.pdf não disponível'.format(file_name))
     
+    def print_table(self):
+        r = self.get_page('quadro_horario')
+
+        soup = BeautifulSoup(r.content, 'html5lib')
+        tbody = soup.find('tbody')
+
+        rows = []
+        for row in tbody.find_all('tr'):
+            r = []
+            for val in row.find_all('td'):
+                a = val.find('a')
+                span = val.find('span', class_='label')
+                if a:
+                    a_dict = a.attrs
+                    a_dict['content'] = ' '.join(a.text.split())
+                    r.append(a_dict)
+                elif span:
+                    r.append(''.join(span.text.split()))
+                else:
+                    r.append('')
+            rows.append(r)
+            
+        for r in rows:
+            print(r)
+
 
 if __name__ == '__main__':
     session = Session()
